2698ecd6a38513f6e523a7127be21ba0dded3821
Mohammad zarak bin kaleem <mughalzarak@gmail.com>
2020-12-16 12:14:29 +0500

    change cluster name

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func index(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", index)
 	fmt.Println("Server starting...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":80", nil)
 }
